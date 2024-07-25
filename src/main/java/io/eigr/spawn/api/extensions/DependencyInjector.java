94fbb62f2c9a6c0558cdfe64e71b5748ce898b95
Adriano Santos <sleipnir@bsd.com.br>
2024-07-25 11:28:13 -0300

    Improvement on injector

--- src/main/java/io/eigr/spawn/api/extensions/DependencyInjector.java
+++ src/main/java/io/eigr/spawn/api/extensions/DependencyInjector.java
@@ -4,5 +4,9 @@ public interface DependencyInjector {
 
     <T extends Object> void bind(Class<T> type, Object instance);
 
+    <T extends Object> void bind(Class<T> type, String alias, Object instance);
+
     <T extends Object> T getInstance(Class<T> type);
+
+    <T extends Object> T getInstanceByAlias(String alias);
 }
--- src/main/java/io/eigr/spawn/api/extensions/SimpleDependencyInjector.java
+++ src/main/java/io/eigr/spawn/api/extensions/SimpleDependencyInjector.java
@@ -6,9 +6,11 @@ import java.util.Map;
 public final class SimpleDependencyInjector implements DependencyInjector {
 
     private final Map<Class<?>, Object> bucket;
+    private final Map<String, Object> aliasBucket;
 
     private SimpleDependencyInjector() {
         this.bucket = new HashMap<>();
+        this.aliasBucket = new HashMap<>();
     }
 
     private static class SimpleDependencyInjectorHelper{
@@ -29,7 +31,22 @@ public final class SimpleDependencyInjector implements DependencyInjector {
     }
 
     @Override
+    public <T> void bind(Class<T> type, String alias, Object instance) {
+        if (this.aliasBucket.containsKey(type)) {
+            throw new IllegalArgumentException("There is already an instance associated with this type in the bucket");
+        }
+
+        this.aliasBucket.put(alias, instance);
+    }
+
+    @Override
     public <T> T getInstance(Class<T> type) {
         return type.cast(this.bucket.get(type));
+    }
+
+    @Override
+    public Object getInstanceByAlias(String alias) {
+        var typeClass = this.aliasBucket.get(alias).getClass();
+        return typeClass.cast(this.aliasBucket.get(alias));
     }
 }
